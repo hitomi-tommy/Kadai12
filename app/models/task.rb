68de07729d66c0c29e50da3544cb186dd0a59242
hitomi kanaya <hitomickeykey@gmail.com>
2020-09-03 16:17:28 +0900

    エラーメッセージ表示機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { maximum: 75 }
   validates :description, presence: true
+  validates :description, length: { maximum: 400 }
 end
