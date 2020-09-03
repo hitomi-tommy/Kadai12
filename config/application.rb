1bc1da191b37615171b516005543e7d07637e0b9
hitomi kanaya <hitomickeykey@gmail.com>
2020-09-03 17:04:45 +0900

    RSpecをインストール

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module TaskApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-
+    config.generators do |g|
+      g.test_framework :rspec,
+                   model_specs: true,
+                   view_specs: false,
+                   helper_specs: false,
+                   routing_specs: false,
+                   controller_specs: false,
+                   request_specs: false
+    end
   end
 end
