34a08928205aa000eec215c08392f338d6ba49e5
hitomi kanaya <hitomickeykey@gmail.com>
2020-09-09 20:19:46 +0900

    タスクに優先順位を設定できる様実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ApplicationRecord
   validates :description, presence: true, length: { maximum: 400 }
   scope :task_name_like, -> name { where('name LIKE ?', "%#{name}%") }
   scope :status, -> status { where(status: status) }
+  enum priority:{ 高: 0, 中: 1, 低: 2 }
 end
