d6e0f66cb22f4ef694fcafa8aceb87a0882b581a
hitomi kanaya <hitomickeykey@gmail.com>
2020-09-09 21:15:57 +0900

    ページネーション追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,21 +2,21 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     if params[:sort_expired]
-      @task = Task.all.order(deadline: :ASC)
+      @tasks = Task.all.order(deadline: :ASC).page(params[:page]).per(10)
     elsif params[:sort_priority]
-      @task = Task.all.order(priority: :ASC)
+      @tasks = Task.all.order(priority: :ASC).page(params[:page]).per(10)
     elsif
-      @task = Task.all.order(created_at: :DESC)
+      @tasks = Task.all.order(created_at: :DESC).page(params[:page]).per(10)
     end
 
 
     if params[:search].present?
       if params[:name_search].present? && params[:status_search].present?
-        @task = Task.status(params[:status_search]).task_name_like(params[:name_search])
+        @tasks = Task.status(params[:status_search]).task_name_like(params[:name_search]).page(params[:page]).per(10)
       elsif params[:name_search].present?
-        @task = Task.task_name_like(params[:name_search])
+        @tasks = Task.task_name_like(params[:name_search]).page(params[:page]).per(10)
       elsif params[:status_search].present?
-        @task = Task.status(params[:status_search])
+        @tasks = Task.status(params[:status_search]).page(params[:page]).per(10)
       end
     end
   end
