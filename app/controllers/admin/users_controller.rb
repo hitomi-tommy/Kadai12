7b4e7b81cd021e9d892d3797f7fb1a2c19bdc080
hitomi kanaya <hitomickeykey@gmail.com>
2020-09-12 22:20:55 +0900

    管理画面のindex作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,20 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def new
+    @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "#{@user.name}を登録しました"
+    else
+      render :new
+    end
   end
 
-  def show
-  end
-
-  def index
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
