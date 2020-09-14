9c03bd865d06eb4b496e1d8fe61c32c5bb7f5430
hitomi kanaya <hitomickeykey@gmail.com>
2020-09-14 11:14:50 +0900

    管理機能を管理者ユーザーだけに利用させる様変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   def authenticate_user
-    # 現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
-    # if @current_user == nil
-    #   flash[:notice] = t('notice.login_needed')
-    #   redirect_to new_session_path
-    # end
+    現在ログイン中のユーザが存在しない場合、ログインページにリダイレクトさせる。
+    if @current_user == nil
+      flash[:notice] = t('notice.login_needed')
+      redirect_to new_session_path
+    end
+  end
+  helper_method :current_user
+  before_action :login_required
+
+  private
+  def login_required
+    redirect_to login_url unless current_user
   end
 end
